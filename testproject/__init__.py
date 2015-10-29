2019fd076157868c6c47050a65658c2f1305bf8f
Rocky Meza <rockymeza@gmail.com>
2015-10-29 19:47:23 +0800

    Fix tests for Django 1.7+

--- testproject/__init__.py
+++ testproject/__init__.py
@@ -3,12 +3,15 @@ import os
 import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.testproject.settings")
 
+import django
 from django.test.utils import get_runner
 from django.conf import settings
 
 
 def runtests():
     # Stolen from django/core/management/commands/test.py
+    if hasattr(django, 'setup'):
+        django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True)
     failures = test_runner.run_tests(['separated'])
